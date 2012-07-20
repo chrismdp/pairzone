4a921a6715c8f0a236fdb2eeb1fa8f929a8ad289
Chris Parsons <chris@thinkcodelearn.com>
2012-07-20 13:04:34 +0100

    more cleanup

--- lib/pairzone.rb
+++ lib/pairzone.rb
@@ -1,13 +1,10 @@
 require 'pairzone/version'
-require 'pairzone/authenticate'
 require 'pairzone/shell'
 require 'pairzone/logger'
 
-require 'pairzone/api/base'
 require 'pairzone/api/pairzone_lifecycle'
 require 'pairzone/api/pairzone_connection'
 require 'pairzone/api/pairzone'
-require 'pairzone/api/user'
 
 require 'pairzone/commands/info'
 require 'pairzone/commands/start'
--- lib/pairzone/commands/info.rb
+++ lib/pairzone/commands/info.rb
@@ -7,18 +7,7 @@ module Pairzone
 
       def execute
         Logger.info("<%= color('Pairzone Account Information', CYAN + BOLD) %>")
-        @user = Pairzone::Api::User.current_user
-        Logger.info(@user)
-        Logger.info("")
-        Logger.debug("Finding information about local pairzone: #{local_pairzone_name}")
-        pairzone = Pairzone::Api::Pairzone.find_by_project_name(local_pairzone_name)
-        Logger.info(pairzone || "No pairzone found for this directory (create one with <%= color('pairzone create', CYAN) %>).")
-      end
-
-      private
-
-      def local_pairzone_name
-        "#{@user.username}-#{@project_name}"
+        Logger.info("TO BE COMPLETED")
       end
     end
   end
