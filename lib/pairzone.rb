d1459b01c07ab60561d5e6b90510d0abff4c3247
Chris Parsons <chris@thinkcodelearn.com>
2012-07-20 14:04:29 +0100

    Reorganised object to pull concerns out of pairzone

--- lib/pairzone.rb
+++ lib/pairzone.rb
@@ -2,8 +2,7 @@ require 'pairzone/version'
 require 'pairzone/shell'
 require 'pairzone/logger'
 
-require 'pairzone/pairzone_lifecycle'
-require 'pairzone/pairzone_connection'
+require 'pairzone/curator'
 require 'pairzone/instance'
 
 require 'pairzone/commands/info'
---
+++ spec/pairzone/curator_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Pairzone::Curator do
+end
+
