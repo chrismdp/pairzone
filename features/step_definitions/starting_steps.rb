ab542cfc46c7853d7dc83a3bccf8ee3e5c5f12aa
Richard Patching <richard@justaddpixels.com>
2012-07-20 12:19:20 +0000

    Pull out helper methods into the dsl

--- features/step_definitions/starting_steps.rb
+++ features/step_definitions/starting_steps.rb
@@ -1,21 +1,3 @@
-def look_for(text)
-  assert_partial_output(text, all_output)
-end
-
-def check_attempt_connect
-  look_for "/usr/local/bin/pairzone-tmux.sh: No such file or directory" # We cannot run the session, but this will show we tried to
-end
-
-def check_didnt_connect
-  Then %{the output should not contain "pairzone-tmux"} # We cannot run the session, but if this shows up we tried to
-end
-
-def check_pairzone_started_for(developer_name)
-  look_for "Starting Pairzone for project 'aruba'..."
-  look_for "Pairzone '#{developer_name}-aruba' started."
-  look_for "Pairzone booted: ip address 127.0.0.1"
-end
-
 Then /^a new pairzone is started for "([^"]*)"$/ do |developer_name|
   check_pairzone_started_for(developer_name)
 end
--- features/support/pairzone_dsl.rb
+++ features/support/pairzone_dsl.rb
@@ -5,5 +5,23 @@ module PairzoneDSL
     run "git add ."
     run "git commit -m 'Initial commit'"
   end
+
+  def look_for(text)
+    assert_partial_output(text, all_output)
+  end
+
+  def check_attempt_connect
+    look_for "/usr/local/bin/pairzone-tmux.sh: No such file or directory" # We cannot run the session, but this will show we tried to
+  end
+
+  def check_didnt_connect
+    Then %{the output should not contain "pairzone-tmux"} # We cannot run the session, but if this shows up we tried to
+  end
+
+  def check_pairzone_started_for(developer_name)
+    look_for "Starting Pairzone for project 'aruba'..."
+    look_for "Pairzone '#{developer_name}-aruba' started."
+    look_for "Pairzone booted: ip address 127.0.0.1"
+  end
 end
 World(PairzoneDSL)
